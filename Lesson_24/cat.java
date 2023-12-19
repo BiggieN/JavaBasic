5b414bbf3623eb4ca7b9f37c6d22765818b7d401
Eugene <gavrilovec.evgen@gmail.com>
2023-12-19 23:10:15 +0300

    Seminar6-final

--- Lesson_24/cat.java
+++ Lesson_24/cat.java
@@ -1,10 +1,31 @@
 package JavaBasic.Lesson_24;
 
-import java.util.List;
-
 public class cat {
     String name;
     int age;
     String poroda;
     String ownerName;
+
+    @Override
+    public String toString() {
+        return "name: " + name + ", age: " + age + ", poroda: " + poroda + ", owner: " + ownerName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj){
+            return true;
+        }
+        if (obj instanceof cat){
+            cat cat = (cat) obj;
+            return name.equals(cat.name) && age == cat.age
+                    && poroda.equals(cat.poroda) && ownerName.equals(cat.ownerName);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode() + 7*age + 11*poroda.hashCode() + 17*ownerName.hashCode();
+    }
 }
--- Lesson_24/main.java
+++ Lesson_24/main.java
@@ -1,5 +1,8 @@
 package JavaBasic.Lesson_24;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /*
  * 1. Продумайте структуру класса Кот. Какие поля и методы будут актуальны для приложения, которое является
  * а) информационной системой ветеринарной клиники
@@ -10,6 +13,60 @@ package JavaBasic.Lesson_24;
 
 public class main {
     public static void main(String[] args) {
-        Cat cat1 = new Cat();
+        Set<cat> cats = new HashSet<>();
+
+        cat cat1 = new cat();
+        cat1.name = "Барсик";
+        cat1.age = 10;
+        cat1.poroda = "Персидская";
+        cat1.ownerName = "Мария Ивановна";
+
+        cat cat2 = new cat();
+        cat2.name = "Мурзик";
+        cat2.age = 9;
+        cat2.poroda = "Дворняга";
+        cat2.ownerName = "Кирилл Петрович";
+
+        cat cat3 = new cat();
+        cat3.name = "Мурка";
+        cat3.age = 8;
+        cat3.poroda = "Сфинкс";
+        cat3.ownerName = "Семен Иванович";
+
+        cat cat4 = new cat();
+        cat4.name = "Рыжик";
+        cat4.age = 9;
+        cat4.poroda = "Британская";
+        cat4.ownerName = "Вася";
+
+        cat cat5 = new cat();
+        cat5.name = "Рыжик";
+        cat5.age = 9;
+        cat5.poroda = "Британская";
+        cat5.ownerName = "Вася";
+
+        cats.add(cat1);
+        cats.add(cat2);
+        cats.add(cat3);
+        cats.add(cat4);
+        cats.add(cat5);
+
+        printSet(findByAge(cats, 9));
+    }
+
+    static Set<cat> findByAge(Set<cat> cats, int minAge){
+        Set<cat> res = new HashSet<>();
+        for (cat cat: cats){
+            if (cat.age >= minAge){
+                res.add(cat);
+            }
+        }
+        return res;
+    }
+
+    static void printSet(Set<cat> cats){
+        for (cat cat: cats){
+            System.out.println(cat);
+        }
     }
 }
